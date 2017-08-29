c5971bb4c2240ca15514e70bc3c73c719911b7f2
admin <aranadan@gmail.com>
2017-08-29 11:46:40 +0300

    add first coordinates

--- app/src/main/java/com/example/android/quakereport/MapsActivity.java
+++ app/src/main/java/com/example/android/quakereport/MapsActivity.java
@@ -40,7 +40,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng ozernovskiy = new LatLng(52.0498,157.122);
+
+
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions().position(ozernovskiy).zIndex(144.5f).title("74km NE of Ozernovskiy, Russia"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ozernovskiy));
+
     }
 }
