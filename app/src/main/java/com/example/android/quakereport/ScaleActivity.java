445ca68c160641dfabb71459916a344ea6d378b5
aranadan <aranadan@gmail.com>
2017-09-28 16:21:56 +0300

    add vibrate on scroll scale

--- app/src/main/java/com/example/android/quakereport/ScaleActivity.java
+++ app/src/main/java/com/example/android/quakereport/ScaleActivity.java
@@ -1,9 +1,10 @@
 package com.example.android.quakereport;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Vibrator;
 import android.widget.NumberPicker;
 
 public class ScaleActivity extends Activity {
@@ -12,21 +13,7 @@ public class ScaleActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scale);
-        //ListView listView = (ListView) findViewById(R.id.scale_list);
-        //create scale array for earthquake magnitude
-        /*final ArrayAdapter<Integer> scaleAdapter = new ArrayAdapter<Integer>(this.getApplicationContext(),
-                android.R.layout.simple_list_item_1,new Integer[]{1,2,3,4,5,6,7,8,9,10});*/
-        //listView.setAdapter(scaleAdapter);
-        //listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-           /* @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intentResultItem = new Intent();
-                intentResultItem.putExtra("scale",parent.getItemAtPosition(position).toString());
-                setResult(RESULT_OK,intentResultItem);
-                finish();
-            }
-        });*/
+        final Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         final NumberPicker numberPicker = (NumberPicker) findViewById(R.id.numberPicker);
         numberPicker.setMaxValue(10);
         numberPicker.setMinValue(1);
@@ -35,7 +22,11 @@ public class ScaleActivity extends Activity {
         numberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+
+
                 EarthquakeActivity.magnitude = newVal;
+                //Vibrate for 30milliseconds
+                vibrator.vibrate(30);
             }
         });
 
